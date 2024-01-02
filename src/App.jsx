5881957cfd6bfb9785130b253d7dc50b9a93c65b
ELRODRY <rodrigoezequielolivaresvcp@gmail.com>
2024-01-02 15:44:19 -0300

    primeras pruebas input

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,54 @@
-import { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-  const [inputLeft, setInputLeft] = useState(
-    window.localStorage.getItem("inputLeft") ?? 0
-  );
-  const [inputRight, setInputRight] = useState(
-    window.localStorage.getItem("inputRight") ?? 0
-  );
-  const [result, setResult] = useState(
-    window.localStorage.getItem("resultValue")
-  );
 
-  useEffect(() => {
-    window.localStorage.setItem("resultValue", result);
-  }, [result]);
-  useEffect(() => {
-    window.localStorage.setItem("inputLeft", inputLeft);
-  }, [inputLeft]);
-  useEffect(() => {
-    window.localStorage.setItem("inputRight", inputRight);
-  }, [inputRight]);
+  const handleinputLeft = (event) => {
+    event.preventDefault()
+    const form = event.target;
+    const formData = new FormData(form);
+    // Iterar sobre los datos del formulario
+    formData.forEach((value, name) => {
+      console.log(`${name}: ${value}`);
 
-  const onChangeHandler = (value, inputs) => {
-    if (inputs == "left") {
-      setInputLeft(value);
-    } else {
-      setInputRight(value);
-    }
+    });
   };
-
-  const add = () => {
-    setResult(Number(inputLeft) + Number(inputRight));
-  };
-  const rest = () => {
-    setResult(inputLeft - inputRight);
-  };
-
-  const multiply = () => {
-    setResult(inputLeft * inputRight);
-  };
-
-  const divide = () => {
-    setResult(inputLeft / inputRight);
-  };
-  const clear = () => {
-    setResult(0), setInputLeft(""), setInputRight("");
-  };
+  /* const add = () => {
+     setResult(Number(inputLeft) + Number(inputRight));
+   };
+   const rest = () => {
+     setResult(inputLeft - inputRight);
+   };
+ 
+   const multiply = () => {
+     setResult(inputLeft * inputRight);
+   };
+ 
+   const divide = () => {
+     setResult(inputLeft / inputRight);
+   };
+   const clear = () => {
+     setResult(0), setInputLeft(""), setInputRight("");
+   };*/
 
   return (
     <div id="container">
-      <input
-        id="left"
-        type="number"
-        value={inputLeft}
-        onChange={(e) => onChangeHandler(e.target.value, "left")}
-      />
-      <div id="buttons">
+      <form onSubmit={(e) => { handleinputLeft(e) }}>
+
+        <input
+          name="left"
+          type="number"
+        />
+
+        <input
+          name="right"
+          type="number"
+
+
+        />
+
+      </form>
+      {/* <div id="buttons">
         <div id="" addRest>
           <button onClick={add}>+</button>
           <button onClick={rest}>-</button>
@@ -66,16 +58,11 @@ function App() {
         <div id="clear">
           <button onClick={clear}>Clear</button>
         </div>
-      </div>
-      <input
-        id="right"
-        type="number"
-        value={inputRight}
-        onChange={(e) => onChangeHandler(e.target.value, "right")}
-      />
-      <div className="result">
+      </div> */}
+
+      {/* <div className="result">
         <p>{result}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
